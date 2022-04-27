5074ceccf16abb82e4f7ba198f313552e0c9b06a
ShovaSarker <shovasarker01@gmail.com>
2022-04-27 10:01:52 +0600

    added order route in the server and jwt token authorization added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const cors = require('cors')
 require('dotenv').config()
 
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthrized Access' })
+  }
+
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden Access' })
+    }
+    console.log('decoded', decoded)
+    req.decoded = decoded
+    next()
+  })
+}
 
 app.get('/', (req, res) => {
   res.send('Genius Car Service Running')
@@ -25,6 +43,16 @@ const run = async () => {
     await client.connect()
 
     const serviceCollection = client.db('geniusCar').collection('service')
+    const orderCollection = client.db('geniusCar').collection('order')
+
+    //AUTH Getting JWT Token
+    app.post('/login', async (req, res) => {
+      const user = req.body
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d',
+      })
+      res.send({ accessToken })
+    })
 
     app.get('/services', async (req, res) => {
       const query = {}
@@ -50,6 +78,24 @@ const run = async () => {
       const id = req.params
       const query = { _id: ObjectId(id) }
       const result = await serviceCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.get('/order', verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email
+      const query = req.query
+      if (decodedEmail === query.email) {
+        const cursor = orderCollection.find(query)
+        const result = await cursor.toArray()
+        res.send(result)
+      } else {
+        res.status(403).send({ message: 'Forbidden Access' })
+      }
+    })
+
+    app.post('/order', async (req, res) => {
+      const order = req.body
+      const result = await orderCollection.insertOne(order)
       res.send(result)
     })
   } finally {
